5b32e0a5575c4f3de2012fb4d808732fb255f66f
Televnoy <vtelevnoy@gmail.com>
2026-03-08 23:36:53 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - конфигурация для Яндекс.Облака
+window.YANDEX_CLOUD_CONFIG = {
+    accessKeyId: 'YCAJE88wpdu7ptZmgrq5DJNo_',
+    secretAccessKey: 'YCNDZkfqM7nupiKb-CCL33soTBvllReCi2jx3cca' // !!! ЗАМЕНИТЕ НА НОВЫЙ КЛЮЧ ПОСЛЕ ПЕРЕВЫПУСКА !!!
+};
